022346c9410be515873c76fd58c04ef5d392c068
Audrow Nash <audrow@hey.com>
2021-12-10 07:25:52 -0800

    Specify cac version used
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { walk } from "https://deno.land/std@0.117.0/fs/walk.ts";
 export { dirname } from "https://deno.land/std@0.117.0/path/mod.ts";
-export { cac } from "https://unpkg.com/cac/mod.ts";
+export { cac } from "https://unpkg.com/cac@6.7.12/mod.ts";
